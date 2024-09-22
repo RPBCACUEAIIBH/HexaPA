d970ab4d558aae8f76aec2b209340928b16812ad
Tibor Áser Veres (RPBCACUEAIIBH) <admin@osrc.rip>
2024-09-22 17:16:31 +0300

    Debugging some import issue... one instance of HexaPA works fine, the cloned one gives attribute error on LogTrace attributes...

--- HexaLibPython/__init__.py
+++ HexaLibPython/__init__.py
@@ -5,8 +5,13 @@ import os
 import glob
 
 DirectoryPath = os.path.dirname (__file__)
-for ModulePath in glob.glob(os.path.join (DirectoryPath, "*.py")):
-    if ModulePath == __file__ or os.path.basename (ModulePath).startswith ("_"):
-        continue
-    ModuleName = os.path.basename (ModulePath)[:-3]
-    exec (f"from .{ModuleName} import *")
+for ModulePath in glob.glob (os.path.join (DirectoryPath, "*.py")):
+	if ModulePath == __file__ or os.path.basename (ModulePath).startswith ("_"):
+		continue
+	ModuleName = os.path.basename (ModulePath)[:-3]
+	print (f"Debug: Importing {ModuleName}...")
+	try:
+		exec (f"from .{ModuleName} import *")
+	except Exception as e:
+		print (__file__)
+		print (f"Failed to import module {ModuleName}: {e}")
